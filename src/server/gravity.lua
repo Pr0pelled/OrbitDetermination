1bfb846566dd38353e8f0c98cedfa96dab828409
Pr0pelled <pr0pelled.12@gmail.com>
2023-10-26 01:56:16 -0400

    Added a class to handle gravity physics

---
+++ src/server/gravity.lua
@@ -0,0 +1,24 @@
+--Calculates gravity using newtons equations
+
+--Services
+local RunService = game:GetService("RunService")
+
+--Bodies
+local Big = workspace:WaitForChild("Big")
+local Small = workspace:WaitForChild("Small")
+
+--Constants
+local G = 6.67e-11
+local massMultiplier = 10e10
+
+--Functions
+local function calculateGravity(deltaTime: number)
+	--F = G * (m1 * m2) / r^2
+	local dist = Big.Position - Small.Position
+	local force = (G * (Big.Mass * massMultiplier) / (dist.Magnitude^2)) * dist.Unit
+	Small.AssemblyLinearVelocity += (force * deltaTime)
+end
+
+RunService:BindToRenderStep("CalculateGravity", Enum.RenderPriority.Last.Value, calculateGravity)
+
+return nil
