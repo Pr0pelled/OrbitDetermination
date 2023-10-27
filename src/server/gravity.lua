03e5bf26e5177abdc9c65e1d78ba3132953043ab
Pr0pelled <pr0pelled.12@gmail.com>
2023-10-27 00:26:39 -0400

    Added comments

--- src/server/gravity.lua
+++ src/server/gravity.lua
@@ -15,8 +15,8 @@ local massMultiplier = 10e10
 local function calculateGravity(deltaTime: number)
 	--F = G * (m1 * m2) / r^2
 	local dist = Big.Position - Small.Position
-	local force = (G * (Big.Mass * massMultiplier) / (dist.Magnitude^2)) * dist.Unit
-	Small.AssemblyLinearVelocity += (force * deltaTime)
+	local force = (G * (Big.Mass * massMultiplier) / (dist.Magnitude^2)) * dist.Unit --Ignoring the small mass
+	Small.AssemblyLinearVelocity += (force * deltaTime) --Again, ignoring the small mass
 end
 
 RunService:BindToRenderStep("CalculateGravity", Enum.RenderPriority.Last.Value, calculateGravity)
