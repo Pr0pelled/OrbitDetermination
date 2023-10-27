6c375a7f4a8cffe5f5cb48088b8512c71b30f306
Pr0pelled <pr0pelled.12@gmail.com>
2023-10-27 00:31:55 -0400

    Cleaned Up, Added comments

--- src/server/orbitDrawer.lua
+++ src/server/orbitDrawer.lua
@@ -11,7 +11,8 @@ type orbitInfo = {
 	["LongitudeOfAscendingNode"]: number,
 	["ArgumentOfPeriapsis"]: number,
 	["StandardGravitationalParameter"]: number,
-	["SpecificOrbitalEnergy"]: number
+	["SpecificOrbitalEnergy"]: number,
+	["OrbitUpVector"]: Vector3,
 }
 
 local thisModule = {}
@@ -40,6 +41,7 @@ local function createNode(position: Vector3, color: Color3)
 	node.CFrame = CFrame.new(position)
 	node.Size = Vector3.new(0.5, 0.5, 0.5)
 	node.Color = color
+	node.Material = Enum.Material.SmoothPlastic
 	node.CanCollide = false
 	node.Anchored = true
 	node.Shape = "Ball"
@@ -47,16 +49,25 @@ local function createNode(position: Vector3, color: Color3)
 	return node
 end
 
+--Polar-coordinate formula for Kepler Orbits
 local function getRadius(semiMajorAxis: number, eccentricity: number, theta:number)
 	return (semiMajorAxis * (1 - eccentricity^2)) / (1 + eccentricity * math.cos(theta))
 end
 
+--Using trig to get the position in Cartesian Coordinates
 local function getPositionFromRadius(radius: number, theta: number)
 	return Vector3.new(radius * math.sin(theta), 0, radius * math.cos(theta))
 end
 
+--Checks to assure a given position is not past a plane perpendicaly to the periapsis direction
+local function isWithinBounds(position: Vector3, bound: Vector3): boolean
+	return (position - bound):Dot(bound) < bound.Magnitude
+end
+
 --Module  methods
 function thisModule.drawOrbit(info: orbitInfo, numPoints: number, color: Color3?)
+
+	--Creating a parent part for the entire display
 	local mainPart = Instance.new("Part")
 	mainPart.Size = Vector3.one
 	mainPart.CFrame = CFrame.new(0, 0, 0)
@@ -64,39 +75,35 @@ function thisModule.drawOrbit(info: orbitInfo, numPoints: number, color: Color3?
 	mainPart.Anchored = true
 	mainPart.CanCollide = false
 
-	local turnValue = 0
-	if info.ArgumentOfPeriapsis > math.pi then
-		turnValue = 2 * math.pi - info.ArgumentOfPeriapsis
-	else
-		turnValue = math.pi - info.ArgumentOfPeriapsis
+	--Calculating an offset for the orbit
+	local turnValue = math.pi - info.ArgumentOfPeriapsis
+
+	--For hyperbolas
+	if info.Eccentricity > 1 then
+		turnValue -= math.pi
 	end
 
-	print(info.ArgumentOfPeriapsis - (math.pi/4), info.ArgumentOfPeriapsis + (math.pi/4))
-
+	--Calculating the position of a periapsis for bound-checking
 	local periapsisPosition = getPositionFromRadius(getRadius(info.SemiMajorAxis, info.Eccentricity, 0), 0 - turnValue)
-	local apoapsisPosition = getPositionFromRadius(getRadius(info.SemiMajorAxis, info.Eccentricity, math.pi), math.pi - turnValue)
-	local adjustedEccentricityVector = apoapsisPosition - periapsisPosition
-	print(adjustedEccentricityVector)
 
 	local lastAttachment: Attachment, firstAttachment: Attachment = nil
 	for i = 0, numPoints, 1 do
-		local theta
-		if info.Eccentricity > 1 then
-			theta = (math.pi + ((2 * math.pi) * (i / numPoints))) % (2 * math.pi)
-		else
-			theta = ((2 * math.pi) * (i / numPoints))
-		end
-		local r = getRadius(info.SemiMajorAxis, info.Eccentricity, theta + turnValue)
-		local pos = getPositionFromRadius(r, theta)
-		if (math.abs(pos.X) < math.abs(periapsisPosition.X) or math.abs(pos.Y) < math.abs(periapsisPosition.Y)) then --Trying to make it so that only the pertinent half of the hyperbola is shown.
+		local theta = (2 * math.pi) * (i / numPoints) --The angle
+		local r = getRadius(info.SemiMajorAxis, info.Eccentricity, theta + turnValue) --The radius
+		local pos = getPositionFromRadius(r, theta) --The position of this attachment
+
+		--Determining whether to draw this attachment
+		if info.Eccentricity < 1 or isWithinBounds(pos, periapsisPosition) then --Trying to make it so that only the pertinent half of the hyperbola is shown.
 			local attachment = createAttachment(pos, theta)
 			attachment.Parent = mainPart
 			if lastAttachment then
 				createBeam(lastAttachment, attachment, 0.1, color or Color3.new(0, 0, 0)).Parent = mainPart
-			else
+			elseif firstAttachment == nil then
 				firstAttachment = attachment
 			end
 			lastAttachment = attachment
+		else
+			lastAttachment = nil
 		end
 	end
 	if info.Eccentricity <= 1 then
@@ -104,23 +111,30 @@ function thisModule.drawOrbit(info: orbitInfo, numPoints: number, color: Color3?
 	end
 
 	--Creating different important nodes
+	local nodePos: Vector3 = nil
 
 	--Periapsis
-	createNode(getPositionFromRadius(getRadius(info.SemiMajorAxis, info.Eccentricity, 0), 0 - turnValue), Color3.fromRGB(0, 255, 0)).Parent = mainPart
+	createNode(periapsisPosition, Color3.fromRGB(0, 255, 0)).Parent = mainPart --I don't have to check whether it's in bounds; it always will be
 
 	--Apoapsis
-	if info.Eccentricity < 1 then
-		createNode(getPositionFromRadius(getRadius(info.SemiMajorAxis, info.Eccentricity, math.pi), math.pi - turnValue), Color3.fromRGB(255, 0, 0)).Parent = mainPart
+	nodePos = getPositionFromRadius(getRadius(info.SemiMajorAxis, info.Eccentricity, math.pi), math.pi - turnValue)
+	if info.Eccentricity < 1 or isWithinBounds(nodePos, periapsisPosition) then
+		createNode(nodePos, Color3.fromRGB(255, 0, 0)).Parent = mainPart
 	end
 
 	--Ascending Node
-	createNode(getPositionFromRadius(getRadius(info.SemiMajorAxis, info.Eccentricity, -info.ArgumentOfPeriapsis), -info.ArgumentOfPeriapsis - turnValue), Color3.fromRGB(255, 0, 255)).Parent = mainPart
+	nodePos = getPositionFromRadius(getRadius(info.SemiMajorAxis, info.Eccentricity, -info.ArgumentOfPeriapsis), -info.ArgumentOfPeriapsis - turnValue)
+	if info.Eccentricity < 1 or isWithinBounds(nodePos, periapsisPosition) then
+		createNode(nodePos, Color3.fromRGB(255, 0, 255)).Parent = mainPart
+	end
 
 	--Descending Node
-	createNode(getPositionFromRadius(getRadius(info.SemiMajorAxis, info.Eccentricity, math.pi - info.ArgumentOfPeriapsis), math.pi - info.ArgumentOfPeriapsis - turnValue), Color3.fromRGB(128, 0, 255)).Parent = mainPart
+	nodePos = getPositionFromRadius(getRadius(info.SemiMajorAxis, info.Eccentricity, math.pi - info.ArgumentOfPeriapsis), math.pi - info.ArgumentOfPeriapsis - turnValue)
+	if info.Eccentricity < 1 or isWithinBounds(nodePos, periapsisPosition) then
+		createNode(nodePos, Color3.fromRGB(128, 0, 255)).Parent = mainPart
+	end
 
-	mainPart.Parent = workspace
-
+	--Returning the whole orbit part
 	return mainPart
 end
 
